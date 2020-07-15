d187b99db24c8cb5f8edf07cdbcd76dbba6efc78
caihongquan <caihongquan@medlinker.com>
2020-07-15 11:58:52 +0800

    feat:test

---
+++ generator/template/src/components/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+
+  },
+  data() {
+     return {
+
+     };
+  },
+  components: {
+
+  },
+  methods: {
+
+  },
+  created() {
+
+  },
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ generator/template/src/vuex/index.js
@@ -0,0 +1,1 @@
+congole.log('vuex')
